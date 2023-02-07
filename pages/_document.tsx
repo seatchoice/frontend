e1d2fb1001fe5d25b3ae16d65147cbe8473a3a72
minkyung00 <mksin00@naver.com>
2023-02-07 21:03:41 +0900

    conf: add global dark mode background, text color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="ko-KR">
+    <Html lang="ko-KR" className="dark">
       <Head />
-      <body>
+      <body className="bg-light-bg text-light-text dark:bg-dark-bg dark:text-dark-text">
         <Main />
         <NextScript />
       </body>
