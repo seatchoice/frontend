61be7c41d20d383c61fc0be3d75285fac464151a
minkyung00 <mksin00@naver.com>
2023-02-07 21:02:32 +0900

    feat: create KakaoLoginButton component

---
+++ domain/login/components/KakaoLoginButton/index.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+import { KAKAO_AUTH_URL } from "@/domain/login/constants/kakaoAuth";
+
+import { KakaoIcon } from "./KakaoIcon";
+
+export function KakaoLoginButton() {
+  return (
+    <Link
+      href={KAKAO_AUTH_URL}
+      className="flex gap-2 p-3 px-6 bg-[#FEE500] text-black rounded-lg active:translate-y-px"
+    >
+      <KakaoIcon />
+      카카오로 로그인하기
+    </Link>
+  );
+}
