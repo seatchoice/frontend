cdb0aee72e7dfb6219694691ed603385aa72d5e8
PisoDev77 <pisodev0427@gmail.com>
2023-02-09 14:01:43 +0900

    conf: font `pretendard` added
    public/fonts
    	- Pretendard-Black.woff2
    	- Pretendard-Bold.woff2
    	- Pretendard-ExtraBold.woff2
    	- Pretendard-ExtraLight.woff2
    	- Pretendard-Light.woff2
    	- Pretendard-Medium.woff2
    	- Pretendard-Regular.woff2
    	- Pretendard-SemiBold.woff2
    	- Pretendard-Thin.woff2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./domain/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './domain/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     fontSize: {
-      sm: "0.75rem",
-      base: "1rem",
-      lg: "1.25rem",
-      xl: "1.5rem",
-      "2xl": "1.75rem",
-      "3xl": "2rem",
-      "4xl": "3rem",
+      sm: '0.75rem',
+      base: '1rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '1.75rem',
+      '3xl': '2rem',
+      '4xl': '3rem',
     },
     extend: {
       colors: {
         primary: colors.violet,
+      },
+      fontFamily: {
+        pretendardBlack: 'pretendard-black',
+        pretendardBold: 'pretendard-bold',
+        pretendardExtraBold: 'pretendard-extra-bold',
+        pretendardExtraLight: 'pretendard-extra-light',
+        pretendardLight: 'pretendard-light',
+        pretendardMedium: 'pretendard-medium',
+        pretendardRegular: 'pretendard-regular cursive',
+        pretendardSemiBold: 'pretendard-semi-bold',
+        pretendardThin: 'pretendard-thin',
       },
     },
   },
