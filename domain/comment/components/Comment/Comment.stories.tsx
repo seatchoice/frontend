824a8c15471db040d8dd76bf651dd238893c790d
minkyung00 <mksin00@naver.com>
2023-02-12 21:18:05 +0900

    feat: create Comment component

---
+++ domain/comment/components/Comment/Comment.stories.tsx
@@ -0,0 +1,30 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Comment } from ".";
+
+export default {
+  component: Comment,
+  title: "Comment/Components/Comment",
+  parameters: {
+    design: {
+      type: "figma",
+      url: "figmaURL",
+    },
+  },
+} as ComponentMeta<typeof Comment>;
+
+const Template: ComponentStory<typeof Comment> = (args) => (
+  <Comment {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  comment: {
+    id: 1,
+    nickname: "nickname",
+    content: "content",
+    updatedAt: "2022-02-01",
+    likeAmount: 10,
+  },
+};
---
+++ domain/comment/components/Comment/index.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+
+import { Button, LikeButton, Text } from "@/components";
+
+type CommentProps<T extends React.ElementType> = Component<T> & {
+  comment: _Comment;
+};
+
+export function Comment({
+  comment,
+  className,
+  children,
+  ...props
+}: CommentProps<"article">) {
+  const { nickname, content, updatedAt, likeAmount } = comment;
+  return (
+    <article className="rounded-lg">
+      <div className="flex justify-between items-center mb-2">
+        <div className="flex items-center gap-2">
+          <Image
+            src="https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+            alt="프로필 사진"
+            width={24}
+            height={24}
+            className="w-6 h-6 rounded-full"
+          />
+          <span>{nickname}</span>
+          <Text className="text-sm text-gray-500">
+            <time dateTime={updatedAt} title={updatedAt}>
+              {updatedAt}
+            </time>
+          </Text>
+        </div>
+        <ul className="flex">
+          <li>
+            <Button className="bg-transparent">편집</Button>
+          </li>
+          <li>
+            <Button className="bg-transparent">삭제</Button>
+          </li>
+        </ul>
+      </div>
+      <Text>{content}</Text>
+      <footer className="flex items-center mt-4 space-x-4">
+        <LikeButton className="text-sm">{likeAmount}</LikeButton>
+      </footer>
+    </article>
+  );
+}
