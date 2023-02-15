03cbe96543fb77cb7851ff273feedc8714486a30
minkyung00 <mksin00@naver.com>
2023-02-16 01:41:28 +0900

    refactor: seperate component from ReviewForm
    - SeatSelect
    - ImageUploadButton
    - ImagePreview

--- domain/review/components/ReviewForm/index.tsx
+++ domain/review/components/ReviewForm/index.tsx
@@ -1,14 +1,15 @@
 import { useRouter } from "next/router";
 import { useRef, useState } from "react";
 
-import { Text, Button, Rating, Textarea, Icon, Select } from "@/components";
-import { SEATS } from "@/constants";
-import { getSections, getRows, getSeatNumbers } from "../../utils";
+import { Text, Button, Rating, Textarea } from "@/components";
+import { SeatSelect } from "./SeatSelect";
+import { ImagePreview } from "./ImagePreview";
+import { ImageUploadButton } from "./ImageUploadButton";
 import { useCreateReviewMutation } from "../../hooks/query";
 
 type ImageFiles = {
   file: Blob;
-  previewUrl: string;
+  imagePreviewUrl: string;
 }[];
 
 type ReviewFormProps<T extends React.ElementType> = Component<T> & {
@@ -30,57 +31,7 @@ export function ReviewForm({ children, ...props }: ReviewFormProps<"form">) {
   const [rating, setRating] = useState(0);
   const [images, setImages] = useState<ImageFiles>([]);
 
-  const seatInfo: Array<{
-    id: "floor" | "section" | "seatRow" | "seatNumber";
-    title: string;
-    options: Array<{ value: string }>;
-  }> = [
-    {
-      id: "floor",
-      title: "층",
-      options: Object.keys(SEATS).map((floor) => ({ value: floor })),
-    },
-    {
-      id: "section",
-      title: "구역",
-      options: getSections(seat.floor).map((section) => ({ value: section })),
-    },
-    {
-      id: "seatRow",
-      title: "열",
-      options: getRows(seat.floor, seat.section).map((row) => ({
-        value: row,
-      })),
-    },
-    {
-      id: "seatNumber",
-      title: "번호",
-      options: getSeatNumbers(seat.floor, seat.section, seat.seatRow).map(
-        (seatNumber) => ({ value: seatNumber })
-      ),
-    },
-  ];
-
   const { mutate: createReview } = useCreateReviewMutation();
-
-  const handleSelectChange = (
-    e: React.ChangeEvent<HTMLSelectElement>,
-    id: keyof typeof seat
-  ) => {
-    const { floor, section, seatRow } = { ...seat, [id]: e.target.value };
-    const newSection = id === "floor" ? getSections(floor)[0] : section;
-    const newSeatRow =
-      id !== "seatNumber" ? getRows(floor, newSection)[0] : seatRow;
-    const [newSeatNumber] = getSeatNumbers(floor, newSection, newSeatRow);
-
-    setSeat({
-      ...seat,
-      section: newSection,
-      seatRow: newSeatRow,
-      seatNumber: newSeatNumber,
-      [id]: e.target.value,
-    });
-  };
 
   const handleRatingChange = (newRating: number) => {
     setRating(newRating);
@@ -92,7 +43,7 @@ export function ReviewForm({ children, ...props }: ReviewFormProps<"form">) {
     const newImages = Array.from(e.target.files as ArrayLike<File>).map(
       (file) => ({
         file,
-        previewUrl: URL.createObjectURL(file),
+        imagePreviewUrl: URL.createObjectURL(file),
       })
     );
 
@@ -140,20 +91,8 @@ export function ReviewForm({ children, ...props }: ReviewFormProps<"form">) {
       <Text as="h5" className="font-semibold">
         앉았던 자리 선택하기
       </Text>
-      <section className="flex items-center gap-2 mb-4">
-        {seatInfo.map(({ id, title, options }, index) => (
-          <>
-            <Select
-              id={id}
-              options={options}
-              className="max-w-[4rem] text-center"
-              onChange={(e) => handleSelectChange(e, id)}
-              value={seat[id]}
-            />
-            <label htmlFor={id}>{title}</label>
-          </>
-        ))}
-      </section>
+      <SeatSelect seat={seat} setSeat={setSeat} />
+
       <Text as="h5" className="font-semibold">
         자리가 어떠셨나요?
       </Text>
@@ -167,6 +106,7 @@ export function ReviewForm({ children, ...props }: ReviewFormProps<"form">) {
         자세한 후기를 알려주세요
       </Text>
       <Textarea
+        required
         ref={detailReviewRef}
         placeholder={`
         1. 시야는 어땠나요?
@@ -180,43 +120,16 @@ export function ReviewForm({ children, ...props }: ReviewFormProps<"form">) {
       <Text as="h5" className="font-semibold">
         시야 사진을 등록해주세요
       </Text>
-      <ul className="flex flex-wrap gap-5">
-        <label
-          htmlFor="imageFile"
-          className="flex flex-col items-center justify-center h-40 w-40 p-4 rounded-lg bg-light-fg dark:bg-dark-fg cursor-pointer"
-        >
-          <Icon as="camera" size={40} className="fill-primary-500" />
-          <Text>사진 등록하기</Text>
-        </label>
-        <input
-          type="file"
-          id="imageFile"
-          className="hidden"
-          multiple
-          accept="image/*"
-          onChange={handleFileChange}
-        />
-        {images.map(({ file: { name }, previewUrl }, id) => (
-          <li
-            key={name}
-            className="relative inline-block max-w-[150px] rounded-lg"
-          >
-            <img
-              src={previewUrl}
-              alt="업로드된 이미지"
-              className="h-40 w-40 rounded-lg"
-            />
-            <Button
-              as="icon"
-              className="absolute top-1 right-1 rounded-full bg-primary-500"
-              onClick={() => handleImageDeleteButton(id)}
-            >
-              <Icon as="close" size={20} />
-            </Button>
-            <Text className="truncate">{name}</Text>
-          </li>
+      <div className="flex flex-wrap gap-5">
+        <ImageUploadButton onChange={handleFileChange} />
+        {images.map(({ imagePreviewUrl }, id) => (
+          <ImagePreview
+            key={imagePreviewUrl}
+            imagePreviewUrl={imagePreviewUrl}
+            onDeleteClick={() => handleImageDeleteButton(id)}
+          />
         ))}
-      </ul>
+      </div>
 
       <Button>후기 공유하기</Button>
     </form>
