a9bb35601618d0a1e94ffad94cceb7143eca5dd6
minkyung00 <mksin00@naver.com>
2023-02-16 11:43:03 +0900

    feat: add router change when create review

--- domain/review/hooks/query/useCreateReviewMutation.ts
+++ domain/review/hooks/query/useCreateReviewMutation.ts
@@ -2,6 +2,7 @@ import { AxiosResponse, AxiosError } from "axios";
 import { UseMutationOptions, useMutation } from "@tanstack/react-query";
 
 import { fileApi } from "@/api";
+import { useRouter } from "next/router";
 
 type ReviewRequest = {
   theaterId: string;
@@ -22,10 +23,15 @@ export const useCreateReviewMutation = (
     ReviewRequest
   >
 ) => {
+  const router = useRouter();
+  const { theater } = router.query;
   return useMutation(
     ({ theaterId, payload }) => createReview({ theaterId, payload }),
     {
       ...options,
+      onSuccess: () => {
+        router.push(`/${theater}`);
+      },
     }
   );
 };
