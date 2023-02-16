751122309f8534964177ce7201498bb522fe0b86
PisoDev77 <pisodev0427@gmail.com>
2023-02-16 14:31:01 +0900

    feat: Change style of Theaters

--- domain/search/components/Theater/index.tsx
+++ domain/search/components/Theater/index.tsx
@@ -1,14 +1,13 @@
 import TheaterCard from './TheaterCard';
 import TheaterImage from './TheaterImage';
 
-export default function Theater(prop) {
+export default function Theater({ theater }) {
   return (
     <div className="flex items-start p-6">
-      <h1 className="text-lg">{prop.caption}</h1>
       <a href="#" className="block shrink-0">
         <TheaterImage />
       </a>
-      <TheaterCard />
+      <TheaterCard theater={theater} />
     </div>
   );
 }
