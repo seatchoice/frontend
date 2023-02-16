86ca500e37e7e9c50e05e54464bf8a21688821e5
minkyung00 <mksin00@naver.com>
2023-02-16 11:41:08 +0900

    feat: add ConfirmModal component

---
+++ domain/review/components/ReviewForm/ConfirmModal/index.tsx
@@ -0,0 +1,39 @@
+import { Button, Modal, Text } from "@/components";
+import { StateAndAction } from "@/types/state";
+
+type ConfirmModal = {
+  seat: {
+    floor: string;
+    section: string;
+    seatRow: string;
+    seatNumber: string;
+  };
+  SubmitButton: React.ReactNode;
+} & StateAndAction<boolean, "showModal">;
+
+export function ConfirmModal({
+  seat,
+  SubmitButton,
+  showModal,
+  setShowModal,
+}: ConfirmModal) {
+  const { floor, section, seatRow, seatNumber } = seat;
+  return (
+    <Modal open={showModal} className="flex flex-col gap-7">
+      <Modal.Header>{`${floor}층 ${section}구역 ${seatRow}열 ${seatNumber}번`}</Modal.Header>
+      <Modal.Body>
+        <Text>위 좌석에 리뷰를 작성하시겠습니까?</Text>
+        <Text>좌석 정보는 수정할 수 없어요.</Text>
+      </Modal.Body>
+      <Modal.Footer>
+        {SubmitButton}
+        <Button
+          onClick={() => setShowModal(false)}
+          className="bg-light-fg dark:bg-dark-fg"
+        >
+          닫기
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
