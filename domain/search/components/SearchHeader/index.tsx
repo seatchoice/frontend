2cf9a9f12d6a14698a9d8f399322b977e135d24a
PisoDev77 <pisodev0427@gmail.com>
2023-02-16 14:42:10 +0900

    feat: 카카오 로그인버튼 검색페이지에서

--- domain/search/components/SearchHeader/index.tsx
+++ domain/search/components/SearchHeader/index.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
-
+import { useRouter } from 'next/router';
 import { Button } from '@/components';
 
 export default function SearchHeader() {
+  const router = useRouter();
   const [dark, setDark] = useState(false);
   let html;
 
@@ -18,10 +19,18 @@ export default function SearchHeader() {
   const handleDark = () => {
     setDark(!dark);
   };
+
+  const handleLogin = () => {
+    router.push('login');
+  };
+
   return (
     <header className="flex justify-between mb-2">
       <h1 className="text-4xl">자리어때</h1>
-      <Button children={'DARK'} onClick={handleDark}></Button>
+      <span>
+        <Button children={'KAKAO LOGIN'} onClick={handleLogin}></Button>
+        <Button children={'DARK'} onClick={handleDark}></Button>
+      </span>
     </header>
   );
 }
