af65b7950ed1bf761e0bfa9cf2118e8991fb91df
minkyung00 <mksin00@naver.com>
2023-02-17 11:36:41 +0900

    feat: add reveiw list api with infinite scroll

---
+++ domain/review/hooks/query/useReviewListQuery.ts
@@ -0,0 +1,40 @@
+import { AxiosResponse } from "axios";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { QUERY_KEYS } from "@/constants";
+
+import { api } from "@/api";
+
+type ReviewListResponse = {
+  data: {
+    content: Array<ReviewWithThumbnail>;
+    empty: boolean;
+    first: boolean;
+    last: boolean;
+    number: number;
+    numberOfElements: number;
+    size: number;
+  };
+};
+
+const getReviewList = (
+  seatId: string,
+  page: number,
+  size: number
+): Promise<AxiosResponse<ReviewListResponse>> => {
+  return api.get(`/reviews?seatId=${seatId}&page=${page}&size=${size}`);
+};
+
+export const useReviewListQuery = (seatId: string, size = 2) => {
+  return useInfiniteQuery(
+    [QUERY_KEYS.REVIEW_LIST, seatId],
+    async ({ queryKey: [, seatId], pageParam = 0 }) => {
+      const { data } = await getReviewList(seatId, pageParam, size);
+      return data;
+    },
+    {
+      getNextPageParam: ({ data: { last, number } }) =>
+        last ? undefined : number + 1,
+    }
+  );
+};
