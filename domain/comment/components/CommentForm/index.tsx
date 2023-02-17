79b30ced9145d1239a9973c31906f88c17a241da
minkyung00 <mksin00@naver.com>
2023-02-17 18:09:55 +0900

    feat: add feature to create comment on CommentForm

--- domain/comment/components/CommentForm/index.tsx
+++ domain/comment/components/CommentForm/index.tsx
@@ -1,5 +1,7 @@
+import { useRef } from "react";
+import { useNextRouter } from "@/hooks/useNextRouter";
 import { Button, Textarea } from "@/components";
-import { tw } from "@/utils/tailwindMerge";
+import { useCreateCommentMutation } from "../../hooks/query";
 
 type CommentFormProps<T extends React.ElementType> = Component<T> & {};
 
@@ -8,13 +10,34 @@ export function CommentForm({
   children,
   ...props
 }: CommentFormProps<"form">) {
+  const {
+    query: { reviewId },
+  } = useNextRouter();
+  const commentRef = useRef<HTMLTextAreaElement>(null);
+  const { mutate: createComment } = useCreateCommentMutation(
+    reviewId as string
+  );
+
+  const handleCommentSubmit = (
+    e: React.FormEvent<HTMLFormElement> & { target: HTMLFormElement }
+  ) => {
+    e.preventDefault();
+
+    const comment = commentRef.current?.value;
+    if (comment) {
+      createComment({ content: comment, reviewId: +(reviewId as string) });
+      e.target.reset();
+    }
+  };
+
   return (
-    <form className={tw("", className)} {...props}>
+    <form onSubmit={handleCommentSubmit}>
       <div className="rounded-lg">
         <label htmlFor="comment" className="sr-only">
           댓글 작성하기
         </label>
         <Textarea
+          ref={commentRef}
           id="comment"
           required
           placeholder="댓글을 작성해주세요."
