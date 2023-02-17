82cbf5b07a3a1a1ce1810d967c38940f42b156a0
minkyung00 <mksin00@naver.com>
2023-02-17 18:13:37 +0900

    feat: add CommentList component

---
+++ domain/comment/components/CommentList/index.tsx
@@ -0,0 +1,21 @@
+import { useNextRouter } from "@/hooks/useNextRouter";
+import { Comment } from "../Comment";
+import { useCommentListQuery } from "../../hooks/query";
+
+export function CommentList() {
+  const {
+    query: { reviewId },
+  } = useNextRouter();
+
+  const {
+    data: { data: commentList },
+  } = useCommentListQuery(reviewId as string);
+
+  return (
+    <>
+      {commentList.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </>
+  );
+}
