3bcacd428d1e1ade2ccc43e82e82eac8e0ef2b05
minkyung00 <mksin00@naver.com>
2023-02-22 19:50:13 +0900

    fix: add router and change to useAuth hook

--- domain/auth/hooks/query/useLogin.ts
+++ domain/auth/hooks/query/useLogin.ts
@@ -1,8 +1,9 @@
 import { AxiosResponse, AxiosError } from "axios";
 import { UseMutationOptions, useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 
 import { api } from "@/api";
-import { STORAGE } from "@/constants";
+import { useAuth } from "../useAuth";
 
 const login = (authCode: string): Promise<AxiosResponse<TokenResponse>> => {
   return api.post(`oauth/kakao/login?code=${authCode}`);
@@ -11,11 +12,15 @@ const login = (authCode: string): Promise<AxiosResponse<TokenResponse>> => {
 export const useLogin = (
   options?: UseMutationOptions<AxiosResponse<TokenResponse>, AxiosError, string>
 ) => {
+  const router = useRouter();
+  const { setToken } = useAuth();
   return useMutation((authCode) => login(authCode), {
     ...options,
     onSuccess: (res) => {
       const accessToken = res.headers.authorization;
-      localStorage.setItem(STORAGE.ACCESS_TOKEN, accessToken);
+      setToken(accessToken);
+
+      router.push("/");
     },
   });
 };
