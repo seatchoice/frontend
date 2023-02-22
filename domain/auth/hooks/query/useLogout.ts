25e9aa598b230dbd31a968a569671af8cac79ca6
minkyung00 <mksin00@naver.com>
2023-02-22 19:50:40 +0900

    feat: add error logic on logout

--- domain/auth/hooks/query/useLogout.ts
+++ domain/auth/hooks/query/useLogout.ts
@@ -2,19 +2,25 @@ import { AxiosResponse, AxiosError } from "axios";
 import { UseMutationOptions, useMutation } from "@tanstack/react-query";
 
 import { api } from "@/api";
-import { STORAGE } from "@/constants";
+import { useAuth } from "../useAuth";
 
 const logout = (): Promise<AxiosResponse> => {
   return api.post(`oauth/kakao/logout`);
 };
 
 export const useLogout = (
-  options?: UseMutationOptions<AxiosResponse, AxiosError>
+  options?: UseMutationOptions<AxiosResponse, AxiosError<ErrorResponse>>
 ) => {
+  const { removeToken } = useAuth();
   return useMutation(logout, {
     ...options,
     onSuccess: () => {
-      localStorage.removeItem(STORAGE.ACCESS_TOKEN);
+      removeToken();
+    },
+    onError: ({ response }) => {
+      if (response?.data.errorCode === "EMPTY_TOKEN") {
+        removeToken();
+      }
     },
   });
 };
