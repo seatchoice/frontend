7096d6825571539241bed5aceaea86b9ca3f7508
minkyung00 <mksin00@naver.com>
2023-02-25 01:30:57 +0900

    fix: change user to nickname

--- components/MainHeader/index.tsx
+++ components/MainHeader/index.tsx
@@ -25,9 +25,9 @@ export function MainHeader() {
             <Icon as="notification" />
           </Link>
           <Dropdown
-            buttonText={user}
+            buttonText={user.nickname}
             items={[
-              { content: user },
+              { content: user.nickname },
               {
                 content: (
                   <Button as="icon" onClick={() => logout()}>
