9b0a6a16218ca754ce46da8281cbaf2aa71aa166
minkyung00 <mksin00@naver.com>
2023-02-25 18:55:57 +0900

    feat: add readAll button on notification

--- domain/notification/components/NotificationHeader/index.tsx
+++ domain/notification/components/NotificationHeader/index.tsx
@@ -1,11 +1,16 @@
 import { Text, Button } from "@/components";
+import { useReadAllMutation } from "../../hooks/query";
 
 export function NotificationHeader() {
+  const { mutate: readAllNotification } = useReadAllMutation();
+
   return (
     <header>
       <Text as="h3">알람 목록</Text>
       <Text as="h4">전체 목록</Text>
-      <Button className="w-full">전체 읽기</Button>
+      <Button onClick={() => readAllNotification()} className="w-full">
+        전체 읽기
+      </Button>
     </header>
   );
 }
