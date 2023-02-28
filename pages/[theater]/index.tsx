df1ba47ee700b2ca8542f9245c57e0c47e853b93
minkyung00 <mksin00@naver.com>
2023-02-28 16:17:59 +0900

    feat: add TheaterMap on theater page

--- pages/[theater]/index.tsx
+++ pages/[theater]/index.tsx
@@ -1,7 +1,6 @@
 import { useNextRouter } from "@/hooks/useNextRouter";
 import { MainHeader, Text } from "@/components";
-import { Seat, Section } from "@/domain/theater/components";
-import { useSeatListQuery } from "@/domain/theater/hooks/query";
+import { TheaterMap } from "@/domain/theater/components";
 
 export default function Theater() {
   const router = useNextRouter();
@@ -9,37 +8,12 @@ export default function Theater() {
     query: { theater },
   } = router;
 
-  const { data: seatList } = useSeatListQuery(theater as string);
-
   return (
     <>
       <MainHeader />
       <div className="text-center">
         <Text as="h1">{theater}</Text>
-        <Text>좌석을 클릭하면 리뷰를 볼 수 있어요</Text>
-        <div className="py-6 bg-light-fg dark:bg-dark-fg">
-          <Text>별점에 따라서 좌석 색깔이 달라져요</Text>
-          <Seat rating={1}>1</Seat>
-          <Seat rating={2}>2</Seat>
-          <Seat rating={3}>3</Seat>
-          <Seat rating={4}>4</Seat>
-          <Seat rating={5}>5</Seat>
-        </div>
-        <>
-          {seatList.map(({ floor, sections }) => (
-            <div
-              key={floor}
-              className="flex flex-col min-h-screen mb-10 overflow-auto"
-            >
-              <Text as="h3" className="text-left">{`${floor}층`}</Text>
-              <div className="flex gap-4">
-                {sections.map(({ section, seats }) => (
-                  <Section key={section} section={section} seats={seats} />
-                ))}
-              </div>
-            </div>
-          ))}
-        </>
+        <TheaterMap />
       </div>
     </>
   );
