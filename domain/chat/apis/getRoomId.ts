eda7c94bb1ab7e30deb3276c8ad6dfa06ee4ee49
PisoDev77 <pisodev0427@gmail.com>
2023-03-02 04:18:31 +0900

    feat: 채팅시 에러
    - roomId가 `-1`인 경우 로그인이 필요한 경우
    - roomId가 `-2`인 경우 서버문 요망인 경우

--- domain/chat/apis/getRoomId.ts
+++ domain/chat/apis/getRoomId.ts
@@ -1,16 +1,30 @@
-import { AxiosResponse } from 'axios';
+import { AxiosResponse, AxiosError } from 'axios';
 import { api } from '@/api';
 
-const getRoomId = (id: string, token: string): Promise<AxiosResponse> =>
-  api.post(
-    '/chat',
-    { theaterId: id },
-    {
-      headers: {
-        Authorization: token,
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-
+const getRoomId = (id: string, token: string): Promise<any | AxiosResponse> =>
+  api
+    .post(
+      '/chat',
+      { theaterId: id },
+      {
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    .catch((error: AxiosError) => {
+      if (error.response?.status === 401) {
+        return {
+          data: {
+            roomId: -1,
+          },
+        };
+      }
+      return {
+        data: {
+          roomId: -2,
+        },
+      };
+    });
 export { getRoomId };
