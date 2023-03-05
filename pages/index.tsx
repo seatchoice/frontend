7b90feccfeba20e7d655e1017ba35f24131a2ce6
minkyung00 <mksin00@naver.com>
2023-03-05 20:05:03 +0900

    feat: add meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,12 @@ export default function Home() {
     <>
       <Head>
         <title>Seat Choice</title>
-        <meta name="description" content="Seat view review site" />
+        <meta name="description" content="공연장 좌석 리뷰 사이트" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Seat Choice" />
+        <meta property="twitter:title" content="Seat Choice" />
+        <meta property="og:description" content="공연장 좌석 리뷰 사이트" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
