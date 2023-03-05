94585126b89104fbe8fa1bf44d4e49f4fc796353
minkyung00 <mksin00@naver.com>
2023-03-05 19:13:39 +0900

    fix: change style of main page

--- domain/search/components/SearchHeader/index.tsx
+++ domain/search/components/SearchHeader/index.tsx
@@ -2,9 +2,10 @@ import { Text } from "@/components";
 
 export default function SearchHeader() {
   return (
-    <header className="flex justify-between mb-2">
-      <Text as="h1" className="text-primary-900 dark:text-primary-100">
-        자리 어때
+    <header className="flex flex-col justify-center items-center py-4">
+      <Text className="text-lg">공연 볼 때</Text>
+      <Text as="h1" className="text-primary-800 dark:text-primary-300">
+        자리어때
       </Text>
     </header>
   );
