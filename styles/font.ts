7218d25ca5dce8e2c730ad97f3a451bd0b77bb81
minkyung00 <mksin00@naver.com>
2023-03-06 00:32:30 +0900

    feat: add medium pretendard font

--- styles/font.ts
+++ styles/font.ts
@@ -3,6 +3,11 @@ import localFont from "@next/font/local";
 export const pretendard = localFont({
   src: [
     {
+      path: "../public/fonts/Pretendard-Medium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
       path: "../public/fonts/Pretendard-Bold.woff2",
       weight: "700",
       style: "normal",
