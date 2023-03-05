611a4f84a8e23088c68f8c559d95ed8c84e50835
minkyung00 <mksin00@naver.com>
2023-03-05 21:26:54 +0900

    fix: image url on meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,18 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="자리어때" />
         <meta property="og:title" content="자리어때 | Seat Choice" />
-        <meta property="twitter:title" content="자리어때 | Seat Choice" />
         <meta name="og:description" content="공연장 좌석 리뷰 사이트" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/seatchoice/frontend/develop/public/logo.png"
+        />
+        <meta property="twitter:title" content="자리어때 | Seat Choice" />
         <meta name="twitter:description" content="공연장 좌석 리뷰 사이트" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/logo.png" />
-        <meta property="og:image" content="/logo.png" />
+        <meta
+          name="twitter:image"
+          content="https://raw.githubusercontent.com/seatchoice/frontend/develop/public/logo.png"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
